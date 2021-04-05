fec385e2f15dc9a37bdf3d0c6ca751f6ae919ede
altayhunoglu <60903744+altayhunoglu@users.noreply.github.com>
2021-04-05 18:04:21 +0300

    Add files via upload

---
+++ marble.jsx
@@ -0,0 +1,9 @@
+﻿docRef = activeDocument;
+rulerUnits = app.preferences.rulerUnits;
+app.preferences.rulerUnits = Units.PIXELS;
+ 
+ app.doAction("Marble this!", "Marble Texture Generator.atn");
+ 
+
+app.preferences.rulerUnits = rulerUnits;
+
